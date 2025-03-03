2262ac084645a0370d89970538be477aa8bd69b3
cynthia-natalie <cynthianatalie573@gmail.com>
2025-03-03 19:24:27 +0700

    initial fastapi-backend-brevo-api code base

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv() # Load .env file
 
 class Settings: # Default if not set
-    BREVO_API_KEY: str = os.getenv("BREVO_API_KEY", "Your-Brevo-API-Key")
+    BREVO_API_KEY: str = os.getenv("BREVO_API_KEY", "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")
     START_DATE: str = os.getenv("START_DATE", "2025-02-14")
     END_DATE: str = os.getenv("END_DATE", "2025-02-21")
     LOG_FILE: str = os.getenv("LOG_FILE", "output.csv")
